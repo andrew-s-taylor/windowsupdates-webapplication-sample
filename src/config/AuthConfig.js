5b72d127ce5e801950bb02f354ef9377df68f102
andrew-s-taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2023-02-15 17:16:17 +0000

    Update AuthConfig.js

--- src/config/AuthConfig.js
+++ src/config/AuthConfig.js
@@ -2,8 +2,8 @@ import { LogLevel } from "@azure/msal-browser"; //MIT LICENSE < https://github.c
 
 export const msalConfig = {
   auth: {
-    clientId: "CLIENT ID", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
-    authority: "https://login.windows.net/TENANT_ID", //e.g. "https://login.windows.net/4d67e6d4-78f7-438b-8e40-7e408dcfa0ca"
+    clientId: "cb6ba756-3724-488a-9af4-c302662a1f71", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
+    authority: "https://login.windows.net/46cd9aae-0d0e-45e1-83b5-154b8efeb92f", //e.g. "https://login.windows.net/4d67e6d4-78f7-438b-8e40-7e408dcfa0ca"
     redirectUri: "http://localhost:3000/",
   },
   cache: {

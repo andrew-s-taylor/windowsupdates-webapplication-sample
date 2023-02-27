0a58028c7f15bb438f44bc53ece2213de1a2fc25
andrew-s-taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2023-02-27 20:46:37 +0000

    Update AuthConfig.js

--- src/config/AuthConfig.js
+++ src/config/AuthConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "cb6ba756-3724-488a-9af4-c302662a1f71", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
     authority: "https://login.windows.net/46cd9aae-0d0e-45e1-83b5-154b8efeb92f", //e.g. "https://login.windows.net/4d67e6d4-78f7-438b-8e40-7e408dcfa0ca"
-    redirectUri: "http://intunedriverinteg.azurewebsites.net:3000",
+    redirectUri: "https://intunedriverinteg.azurewebsites.net",
   },
   cache: {
     cacheLocation: "sessionStorage",
